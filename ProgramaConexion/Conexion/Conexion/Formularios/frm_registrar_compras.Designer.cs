df39efa4a3fa290f5a8c5e01e2e23847fd1b6de9
ramiroGR <217030227@uppenjamo.edu.mx>
2018-12-05 23:12:35 -0600

    agrege un formulario de registro de compras ya establecido conforme alas clasess
    formulario de registro de venta mejorado

---
+++ ProgramaConexion/Conexion/Conexion/Formularios/frm_registrar_compras.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Conexion.Formularios
+{
+    partial class frm_registrar_compras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_codigo = new System.Windows.Forms.TextBox();
+            this.tb_producto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_cantidad = new System.Windows.Forms.TextBox();
+            this.bt_consultar = new System.Windows.Forms.Button();
+            this.bt_registrar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.tb_cantidad, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tb_producto, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tb_codigo, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bt_registrar, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.bt_consultar, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(865, 292);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(435, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Producto";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tb_codigo
+            // 
+            this.tb_codigo.Location = new System.Drawing.Point(219, 3);
+            this.tb_codigo.Name = "tb_codigo";
+            this.tb_codigo.Size = new System.Drawing.Size(100, 20);
+            this.tb_codigo.TabIndex = 2;
+            // 
+            // tb_producto
+            // 
+            this.tb_producto.Location = new System.Drawing.Point(651, 3);
+            this.tb_producto.Name = "tb_producto";
+            this.tb_producto.Size = new System.Drawing.Size(100, 20);
+            this.tb_producto.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cantidad";
+            // 
+            // tb_cantidad
+            // 
+            this.tb_cantidad.Location = new System.Drawing.Point(219, 76);
+            this.tb_cantidad.Name = "tb_cantidad";
+            this.tb_cantidad.Size = new System.Drawing.Size(100, 20);
+            this.tb_cantidad.TabIndex = 5;
+            // 
+            // bt_consultar
+            // 
+            this.bt_consultar.Location = new System.Drawing.Point(219, 222);
+            this.bt_consultar.Name = "bt_consultar";
+            this.bt_consultar.Size = new System.Drawing.Size(138, 58);
+            this.bt_consultar.TabIndex = 6;
+            this.bt_consultar.Text = "Consular";
+            this.bt_consultar.UseVisualStyleBackColor = true;
+            this.bt_consultar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bt_registrar
+            // 
+            this.bt_registrar.Location = new System.Drawing.Point(435, 222);
+            this.bt_registrar.Name = "bt_registrar";
+            this.bt_registrar.Size = new System.Drawing.Size(138, 58);
+            this.bt_registrar.TabIndex = 7;
+            this.bt_registrar.Text = "Registrar";
+            this.bt_registrar.UseVisualStyleBackColor = true;
+            // 
+            // frm_registrar_compras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 292);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frm_registrar_compras";
+            this.Text = "frm_registrar_compras";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_producto;
+        private System.Windows.Forms.TextBox tb_codigo;
+        private System.Windows.Forms.TextBox tb_cantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bt_consultar;
+        private System.Windows.Forms.Button bt_registrar;
+    }
+}
---
+++ ProgramaConexion/Conexion/Conexion/Formularios/frm_registrar_compras.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conexion.Formularios
+{
+    public partial class frm_registrar_compras : Form
+    {
+        public frm_registrar_compras()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
